73b83aa629d95d39fd00fd2b1464e1a844013094
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro.local>
2023-04-11 21:47:35 -0600

    new defaults and keymaps

--- nvim/lua/Settings/defaults.lua
+++ nvim/lua/Settings/defaults.lua
@@ -3,21 +3,31 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- tab indents
-vim.opt.smartindent=true
+vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 -- colorscheme
-vim.cmd("colorscheme tokyonight-storm")
-
+vim.cmd("colorscheme github_dark_default")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 -- no highlight
-vim.opt.hlsearch= false
+vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
--- scrolloff 
+-- scrolloff
 vim.opt.scrolloff = 8
 
 -- cursor
 vim.opt.guicursor = ""
+
+-- nowrap
+vim.opt.wrap = false
+
+-- no swapfile
+vim.opt.swapfile = false
+
+-- cursorline
+vim.opt.cursorline = true
