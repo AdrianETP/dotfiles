755b9e5eb14908dcea0c5c25d47a22d77f813e96
Adrian Eduardo Treviño Peña <adrian@Adrians-MacBook-Pro-3.local>
2023-07-08 18:41:29 -0500

    remove message on save

--- nvim/lua/Settings/lazy.lua
+++ nvim/lua/Settings/lazy.lua
@@ -137,7 +137,7 @@ return require('lazy').setup({
 
     },
 
-    {
+    --[[ {
         "thePrimeagen/harpoon",
         dependencies = 'nvim-lua/plenary.nvim',
         keys = {
@@ -160,7 +160,7 @@ return require('lazy').setup({
         end
 
 
-    },
+    }, ]]
 
 
     -- lsp zero
@@ -338,7 +338,6 @@ return require('lazy').setup({
         config = function()
             require('Comment').setup()
         end,
-        keys = { "gcc", "gbb", "gc", "gb" }
     },
     -- themes
     -- github
@@ -445,11 +444,24 @@ return require('lazy').setup({
                     bottom_search = false,        -- use a classic bottom cmdline for search
                     command_palette = true,       -- position the cmdline and popupmenu together
                     long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+                    inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = true,        -- add a border to hover docs and signature help
+                },
+                routes = {
+                    {
+                        filter = {
+                            event = "msg_show",
+                            kind = "",
+                            find = "written",
+                        },
+                        opts = { skip = true },
+                    },
                 },
             })
         end
 
     },
+    {
+        'nvim-tree/nvim-tree.lua',
+    }
 })
